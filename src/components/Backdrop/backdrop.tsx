a7663b435771aaafdee38e0a87239646de55e7b1
Matheus Azevedo <matheusoliveiraazevedo@gmail.com>
2025-10-03 15:56:56 -0300

    Correção no tamanho do backdrop do loader

--- src/components/Backdrop/backdrop.tsx
+++ src/components/Backdrop/backdrop.tsx
@@ -7,7 +7,7 @@ type BackdropProps = {
 
 export default function Backdrop({ children, onClick }: BackdropProps) {
     return (
-        <div className="bg-gray-700/20 flex justify-center items-center absolute top-20 left-0 w-full h-full z-15" onClick={onClick}>
+        <div className="bg-gray-700/20 flex justify-center items-center absolute top-0 left-0 w-[100vw] h-[100vh] z-15" onClick={onClick}>
             {children}
         </div>
     )
--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -10,7 +10,7 @@ export default function Loader() {
     
     return(
         <>
-            {loadingLoader &&
+            {!loadingLoader &&
                 <Backdrop >
                     <div className="bg-gray-100/50 flex justify-center items-center rounded-2xl p-4 shadow-2xl z-15">
                         <Spinner size="w-20 h-20" border="border-4" />
